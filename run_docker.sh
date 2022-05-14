f5330f5fbb76b24dfe291681403bf74a6e50923d
Johnny Nguyen <johnny.nguyen541@gmail.com>
2022-05-14 00:38:29 -0700

    Working version

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=sklearn-api .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -d -p 5001:80 sklearn-api
