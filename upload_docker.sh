adff29cd54e57cb49b45fcf226009c334d2f6f9b
Johnny Nguyen <johnny.nguyen541@gmail.com>
2022-05-15 23:41:16 -0700

    created upload_docker.sh, removed unneeded f-string in app.py

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,22 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
+## Variables
+DOCKER_ID="johnnynguyen541"
+TAG_ID="sklearn-api"
+IMAGE_VERSION="v1.0.0"
+
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export DOCKERPATH="$DOCKER_ID/$TAG_ID:$IMAGE_VERSION"
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $DOCKERPATH"
+docker login --username $DOCKER_ID
+docker tag $TAG_ID $DOCKERPATH
 
 # Step 3:
 # Push image to a docker repository
+docker push $DOCKERPATH
